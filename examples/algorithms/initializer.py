ca775c2378a71c1bc770288d8c4a10388137149c
Weichi Yao <wy635@stern.nyu.edu>
2022-06-25 22:48:34 -0500

    Added retrain option

--- examples/algorithms/initializer.py
+++ examples/algorithms/initializer.py
@@ -14,121 +14,141 @@ from algorithms.noisy_student import NoisyStudent
 from configs.supported import algo_log_metrics, losses
 from losses import initialize_loss
 
-def initialize_algorithm(config, datasets, train_grouper, unlabeled_dataset=None):
-    train_dataset = datasets['train']['dataset']
+def initialize_algorithm(
+    config, datasets, 
+    train_grouper, 
+    unlabeled_dataset=None, 
+    retrain=False
+):
     train_loader = datasets['train']['loader']
+    train_dataset = datasets['train']['dataset'] 
     d_out = infer_d_out(train_dataset, config)
 
     # Other config
     n_train_steps = math.ceil(len(train_loader)/config.gradient_accumulation_steps) * config.n_epochs
     loss = initialize_loss(config.loss_function, config)
     metric = algo_log_metrics[config.algo_log_metric]
+    
+    if retrain:
+        d_in = datasets['train']['dataset'][0][0].shape[-1] 
+        if config.algorithm == 'ERM':
+            algorithm = ERM(
+                config=config,
+                d_in=d_in,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                metric=metric,
+                n_train_steps=n_train_steps)
+        else:
+            raise ValueError(f"Algorithm {config.algorithm} not recognized")
+    else:   
+        if config.algorithm == 'ERM':
+            algorithm = ERM(
+                config=config,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                metric=metric,
+                n_train_steps=n_train_steps)
+        elif config.algorithm == 'groupDRO':
+            train_g = train_grouper.metadata_to_group(train_dataset.metadata_array)
+            is_group_in_train = get_counts(train_g, train_grouper.n_groups) > 0
+            algorithm = GroupDRO(
+                config=config,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                metric=metric,
+                n_train_steps=n_train_steps,
+                is_group_in_train=is_group_in_train)
+        elif config.algorithm == 'deepCORAL':
+            algorithm = DeepCORAL(
+                config=config,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                metric=metric,
+                n_train_steps=n_train_steps)
+        elif config.algorithm == 'IRM':
+            algorithm = IRM(
+                config=config,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                metric=metric,
+                n_train_steps=n_train_steps)
+        elif config.algorithm == 'DANN':
+            if unlabeled_dataset is not None:
+                unlabeled_dataset = unlabeled_dataset['dataset']
+                metadata_array = torch.cat(
+                    [train_dataset.metadata_array, unlabeled_dataset.metadata_array]
+                )
+            else:
+                metadata_array = train_dataset.metadata_array
 
-    if config.algorithm == 'ERM':
-        algorithm = ERM(
-            config=config,
-            d_out=d_out,
-            grouper=train_grouper,
-            loss=loss,
-            metric=metric,
-            n_train_steps=n_train_steps)
-    elif config.algorithm == 'groupDRO':
-        train_g = train_grouper.metadata_to_group(train_dataset.metadata_array)
-        is_group_in_train = get_counts(train_g, train_grouper.n_groups) > 0
-        algorithm = GroupDRO(
-            config=config,
-            d_out=d_out,
-            grouper=train_grouper,
-            loss=loss,
-            metric=metric,
-            n_train_steps=n_train_steps,
-            is_group_in_train=is_group_in_train)
-    elif config.algorithm == 'deepCORAL':
-        algorithm = DeepCORAL(
-            config=config,
-            d_out=d_out,
-            grouper=train_grouper,
-            loss=loss,
-            metric=metric,
-            n_train_steps=n_train_steps)
-    elif config.algorithm == 'IRM':
-        algorithm = IRM(
-            config=config,
-            d_out=d_out,
-            grouper=train_grouper,
-            loss=loss,
-            metric=metric,
-            n_train_steps=n_train_steps)
-    elif config.algorithm == 'DANN':
-        if unlabeled_dataset is not None:
-            unlabeled_dataset = unlabeled_dataset['dataset']
-            metadata_array = torch.cat(
-                [train_dataset.metadata_array, unlabeled_dataset.metadata_array]
+            groups = train_grouper.metadata_to_group(metadata_array)
+            group_counts = get_counts(groups, train_grouper.n_groups)
+            group_ids_to_domains = group_counts.tolist()
+            domain_idx = 0
+            for i, count in enumerate(group_ids_to_domains):
+                if count > 0:
+                    group_ids_to_domains[i] = domain_idx
+                    domain_idx += 1
+            group_ids_to_domains = torch.tensor(group_ids_to_domains, dtype=torch.long)
+            algorithm = DANN(
+                config=config,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                metric=metric,
+                n_train_steps=n_train_steps,
+                n_domains = domain_idx,
+                group_ids_to_domains=group_ids_to_domains,
             )
+        elif config.algorithm == 'AFN':
+            algorithm = AFN(
+                config=config,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                metric=metric,
+                n_train_steps=n_train_steps
+            )
+        elif config.algorithm == 'FixMatch':
+            algorithm = FixMatch(
+                config=config,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                metric=metric,
+                n_train_steps=n_train_steps)
+        elif config.algorithm == 'PseudoLabel':
+            algorithm = PseudoLabel(
+                config=config,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                metric=metric,
+                n_train_steps=n_train_steps)
+        elif config.algorithm == 'NoisyStudent':
+            if config.soft_pseudolabels:
+                unlabeled_loss = initialize_loss("cross_entropy_logits", config)
+            else:
+                unlabeled_loss = loss
+            algorithm = NoisyStudent(
+                config=config,
+                d_out=d_out,
+                grouper=train_grouper,
+                loss=loss,
+                unlabeled_loss=unlabeled_loss,
+                metric=metric,
+                n_train_steps=n_train_steps)
         else:
-            metadata_array = train_dataset.metadata_array
-
-        groups = train_grouper.metadata_to_group(metadata_array)
-        group_counts = get_counts(groups, train_grouper.n_groups)
-        group_ids_to_domains = group_counts.tolist()
-        domain_idx = 0
-        for i, count in enumerate(group_ids_to_domains):
-            if count > 0:
-                group_ids_to_domains[i] = domain_idx
-                domain_idx += 1
-        group_ids_to_domains = torch.tensor(group_ids_to_domains, dtype=torch.long)
-        algorithm = DANN(
-            config=config,
-            d_out=d_out,
-            grouper=train_grouper,
-            loss=loss,
-            metric=metric,
-            n_train_steps=n_train_steps,
-            n_domains = domain_idx,
-            group_ids_to_domains=group_ids_to_domains,
-        )
-    elif config.algorithm == 'AFN':
-        algorithm = AFN(
-            config=config,
-            d_out=d_out,
-            grouper=train_grouper,
-            loss=loss,
-            metric=metric,
-            n_train_steps=n_train_steps
-        )
-    elif config.algorithm == 'FixMatch':
-        algorithm = FixMatch(
-            config=config,
-            d_out=d_out,
-            grouper=train_grouper,
-            loss=loss,
-            metric=metric,
-            n_train_steps=n_train_steps)
-    elif config.algorithm == 'PseudoLabel':
-        algorithm = PseudoLabel(
-            config=config,
-            d_out=d_out,
-            grouper=train_grouper,
-            loss=loss,
-            metric=metric,
-            n_train_steps=n_train_steps)
-    elif config.algorithm == 'NoisyStudent':
-        if config.soft_pseudolabels:
-            unlabeled_loss = initialize_loss("cross_entropy_logits", config)
-        else:
-            unlabeled_loss = loss
-        algorithm = NoisyStudent(
-            config=config,
-            d_out=d_out,
-            grouper=train_grouper,
-            loss=loss,
-            unlabeled_loss=unlabeled_loss,
-            metric=metric,
-            n_train_steps=n_train_steps)
-    else:
-        raise ValueError(f"Algorithm {config.algorithm} not recognized")
+            raise ValueError(f"Algorithm {config.algorithm} not recognized")
 
     return algorithm
+
 
 def infer_d_out(train_dataset, config):
     # Configure the final layer of the networks used
